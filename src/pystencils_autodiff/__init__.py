200121531ee7b7bd07b2f54ffc88e5a030ac86b4
Stephan Seitz <stephan.seitz@fau.de>
2019-08-06 18:32:34 +0200

    Add _field_to_tensors

--- src/pystencils_autodiff/__init__.py
+++ src/pystencils_autodiff/__init__.py
@@ -1,12 +1,10 @@
 import pystencils_autodiff.backends
-# from pystencils_autodiff._field_to_tensors import (
-# tf_constant_from_field, tf_placeholder_from_field,
-# tf_scalar_variable_from_field, tf_variable_from_field,
-# torch_tensor_from_field)
+from pystencils_autodiff._field_to_tensors import (
+    tf_constant_from_field, tf_placeholder_from_field, tf_scalar_variable_from_field,
+    tf_variable_from_field, torch_tensor_from_field)
 from pystencils_autodiff.adjoint_field import AdjointField
-from pystencils_autodiff.autodiff import (AutoDiffAstPair, AutoDiffOp,
-                                          create_backward_assignments,
-                                          get_jacobian_of_assignments)
+from pystencils_autodiff.autodiff import (
+    AutoDiffAstPair, AutoDiffOp, create_backward_assignments, get_jacobian_of_assignments)
 
 __all__ = ['backends',
            'AdjointField',
@@ -14,7 +12,6 @@ __all__ = ['backends',
            'create_backward_assignments',
            'AutoDiffOp',
            'AutoDiffAstPair',
-           # "tf_constant_from_field", " tf_placeholder_from_field",
-           # "tf_scalar_variable_from_field", " tf_variable_from_field",
-           # "torch_tensor_from_field"
-           ]
+           "tf_constant_from_field", " tf_placeholder_from_field",
+           "tf_scalar_variable_from_field", " tf_variable_from_field",
+           "torch_tensor_from_field"]
