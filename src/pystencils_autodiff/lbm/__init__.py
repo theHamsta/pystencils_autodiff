0bf6d6fdcc017d9aea8a82df483c9f68e39962e6
Stephan Seitz <stephan.seitz@fau.de>
2019-08-12 14:03:02 +0200

    Move submodule `lbm` to its own repository (will by `adjoint_lbmpy`)

--- src/pystencils_autodiff/lbm/__init__.py
+++
@@ -1,4 +0,0 @@
-from lbmpy.autodiff._autodiff_lbstep import AutoDiffLatticeBoltzmannStep
-from lbmpy.autodiff.adjoint_boundaryconditions import AdjointBoundaryCondition
-
-__all__ = ['AutoDiffLatticeBoltzmannStep', 'AdjointBoundaryCondition']
