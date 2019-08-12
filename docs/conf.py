df3353ca69089330ef3869460a98740a22495fb8
Stephan Seitz <stephan.seitz@fau.de>
2019-08-12 10:48:00 +0200

    Switch to bootstrap_theme

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@ import os
 import shutil
 import sys
 
+import sphinx_bootstrap_theme
+
 doctest_global_setup = '''
 import pystencils
 import numpy as np
@@ -140,9 +142,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 # html_theme = 'sphinx_rtd_theme' #official readthedocs theme used also by Martin
-
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
