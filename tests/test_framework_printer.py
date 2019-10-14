8d97ce18453e468220bc0124ceb65d931701e5b2
Stephan Seitz <stephan.seitz@fau.de>
2019-10-14 10:27:39 +0200

    Parametrize test_framework_printer.py test

--- tests/test_framework_printer.py
+++ tests/test_framework_printer.py
@@ -6,13 +6,13 @@
 
 """
 import pytest
-import sympy as sp
 
 import pystencils
+import sympy as sp
 from pystencils.astnodes import Block
-from pystencils_autodiff.framework_integration.printer import FrameworkIntegrationPrinter
 from pystencils_autodiff.framework_integration.astnodes import (
     DestructuringBindingsForFieldClass, KernelFunctionCall, WrapperFunction, generate_kernel_call)
+from pystencils_autodiff.framework_integration.printer import FrameworkIntegrationPrinter
 
 # TODO
 try:
@@ -87,19 +87,20 @@ def test_wrapper_function():
 
 
 @pytest.mark.skipif(not HAS_INTERPOLATION, reason="")
-def test_generate_kernel_call():
+@pytest.mark.parametrize('target', ('cpu', 'gpu'))
+@pytest.mark.parametrize('with_wrapper', ('with_wrapper', False))
+def test_generate_kernel_call(target, with_wrapper):
     z, y, x = pystencils.fields("z, y, x: [100, 80]")
 
     forward_assignments = pystencils.AssignmentCollection([pystencils.Assignment(
         z[0, 0], LinearInterpolator(x).at((0, 0)) * sp.log(x[0, 0] * y[0, 0]))])
 
-    for target in ('cpu', 'gpu'):
+    if not with_wrapper:
         ast = pystencils.create_kernel(forward_assignments, target=target)
         kernel_call_ast = generate_kernel_call(ast)
         code = FrameworkIntegrationPrinter()(kernel_call_ast)
         print(code)
-
-    for target in ('cpu', 'gpu'):
+    else:
         ast = pystencils.create_kernel(forward_assignments, target=target)
         kernel_call_ast = generate_kernel_call(ast)
         wrapper = WrapperFunction(DestructuringBindingsForFieldClass(kernel_call_ast))
@@ -148,4 +149,3 @@ def test_generate_kernel_call_only_texture_for_one_field():
         wrapper = WrapperFunction(DestructuringBindingsForFieldClass(kernel_call_ast))
         code = FrameworkIntegrationPrinter()(wrapper)
         print(code)
-
